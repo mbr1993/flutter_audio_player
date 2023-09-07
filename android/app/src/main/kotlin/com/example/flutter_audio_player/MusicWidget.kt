46b1a6186cf87b973583bc9a0e3c1b20f3f6c3c7
mbr1993 <boburmannonov93@gmail.com>
2023-09-07 07:27:32 +0500

    android manifest

--- android/app/src/main/kotlin/com/example/flutter_audio_player/MusicWidget.kt
+++ android/app/src/main/kotlin/com/example/flutter_audio_player/MusicWidget.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_audio_player
+package com.codeforany.music_player
 
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
@@ -7,9 +7,10 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.RemoteViews
-
+import com.example.flutter_audio_player.R
 
 class MusicWidget : AppWidgetProvider() {
+
     override fun onUpdate(
         context: Context?,
         appWidgetManager: AppWidgetManager?,
@@ -17,24 +18,24 @@ class MusicWidget : AppWidgetProvider() {
     ) {
         if (appWidgetIds != null) {
             for (appWidgetId in appWidgetIds) {
-                updateAppWidget(context, appWidgetManager, appWidgetIds)
+                updateAppWidget(context, appWidgetManager, appWidgetId)
             }
         }
     }
+}
 
-    private fun updateAppWidget(
-        context: Context?,
-        appWidgetManager: AppWidgetManager?,
-        appWidgetIds: IntArray
-    ) {
-//        val widgetText = context?.getString(R.string.appwidget_text)
-//        val views = RemoteViews(context?.packageName, R.layout.music_widget)
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("app://music_player/play"))
 
-        val pendingIntent = PendingIntent.getActivity(
-            context, 0, intent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
-//        views.setOnClickPendingIntent(R.id.button_play_pause, pendingIntent)
-    }
+internal fun updateAppWidget(
+    context: Context?,
+    appWidgetManager: AppWidgetManager?,
+    appWidgetId: Int
+) {
+
+    val widgetText = context?.getString(R.string.appwidget_text)
+    val views = RemoteViews(context?.packageName, R.layout.music_widget)
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("app://music_player/play"))
+
+    val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+    views.setOnClickPendingIntent(R.id.button_play_pause, pendingIntent)
+    appWidgetManager?.updateAppWidget(appWidgetId, views)
 }
